0ab3d2ae6054a1e4ba4f46e1ebe7259e4243c010
Sayanee <sayanee@gmail.com>
2019-09-05 18:26:16 +0800

    Cleanup BOM script

--- bom.py
+++ bom.py
@@ -1,24 +1,8 @@
-#
-# Example python script to generate a BOM from a KiCad generic netlist
-#
-# Example: Sorted and Grouped CSV BOM
-#
-
-"""
-    @package
-    Generate a Tab delimited list (csv file type).
-    Components are sorted by ref and grouped by value with same footprint
-
-    Spreadsheet columns include:
-    'Designator', 'Value', 'Q', 'Package', 'Category', 'Stock', 'Manufacturer', 'Manufacturer Part No. (Datasheet)', 'Vendor (Vendor link)', 'Unit cost', 'Total cost', 'Minimum Order', 'Description'
-
-    Command line:
-    python "bom.py" *.xml bill_of_materials.csv
-"""
-
 import kicad_netlist_reader
 import csv
 import sys
+
+net = kicad_netlist_reader.netlist(sys.argv[1])
 
 try:
     f = open(sys.argv[2], 'w')
@@ -27,7 +11,6 @@ except IOError:
     print(__file__, ":", e, sys.stderr)
     f = sys.stdout
 
-net = kicad_netlist_reader.netlist(sys.argv[1])
 out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
 
 out.writerow([
@@ -48,40 +31,20 @@ out.writerow([
     'Description'
 ])
 
-# Get all of the components in groups of matching parts + values
-# (see ky_generic_netlist_reader.py)
-grouped = net.groupComponents()
-grand_total = 0.0
-
 def total_cost(unit_cost, quantity):
     if not unit_cost:
         return 0
 
     return float(unit_cost) * int(quantity)
 
-def get_datasheet(datasheet):
-    if datasheet == "~":
-        return None
-
-    return datasheet
-
-def get_mpn(mpn):
-    if len(mpn) == 0:
-        return None
-
-    return mpn
-
-# Output all of the component information
+grouped = net.groupComponents()
 for group in grouped:
     refs = ""
 
-    # Add the reference of every component in the group and keep a reference
-    # to the component so that the other data can be filled in once per group
     for component in group:
         refs += component.getRef() + ", "
         c = component
 
-    # Fill in the component groups common data
     out.writerow([
         refs,
         c.getValue(),
@@ -90,8 +53,8 @@ for group in grouped:
         c.getField("Category"),
         c.getField("Stock"),
         c.getField("Manufacturer"),
-        get_mpn(c.getField("Part No.")),
-        get_datasheet(c.getDatasheet()),
+        c.getField("Part No."),
+        c.getDatasheet(),
         c.getField("Vendor"),
         c.getField("Vendor link"),
         c.getField("Unit cost"),
