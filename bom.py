4410a9e0e7f2e338ad1fd1b0d23533e5b47640e4
Sayanee <sayanee@gmail.com>
2019-09-11 17:10:25 +0800

    Simplify creation of BOM analytics with 1 python script

--- bom.py
+++ bom.py
@@ -17,12 +17,22 @@ out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=
 grand_total_cost = 0.0
 total_items = 0
 uniq_items = 0
+vendor_names = []
 
 def total_cost(unit_cost, quantity):
     if not unit_cost:
         return 0
 
-    return float(unit_cost) * int(quantity)
+    return float("{0:.2f}".format(float(unit_cost) * int(quantity)))
+
+def get_uniq_vendor_names(vendors):
+    uniq_vendors = []
+
+    for x in vendors:
+        if x not in uniq_vendors and x:
+            uniq_vendors.append(x)
+
+    return uniq_vendors
 
 # Write CSV
 out.writerow([
@@ -51,6 +61,11 @@ for group in grouped:
         refs += component.getRef() + ", "
         c = component
 
+    uniq_items += 1
+    total_items += len(group)
+    grand_total_cost += total_cost(c.getField("Unit cost"), len(group))
+    vendor_names.append(c.getField("Vendor"))
+
     out.writerow([
         refs,
         c.getValue(),
@@ -69,14 +84,13 @@ for group in grouped:
         c.getPartName() + ": " + c.getDescription()
     ])
 
-# Write BOM analytics
+# Calculate BOM analytics
 data = {}
-data['bom'] = []
-data['bom'].append({
-    'total': grand_total_cost,
-    'uniq_items': uniq_items,
-    'total_items': total_items
-})
+data['bom-uniq'] = uniq_items
+data['bom-total'] = total_items
+data['bom-cost'] = grand_total_cost
+data['vendors'] = len(get_uniq_vendor_names(vendor_names))
+data['vendor-names'] = get_uniq_vendor_names(vendor_names)
 
 with open('_data/bom.json', 'w') as outfile:
     json.dump(data, outfile, indent=2)
