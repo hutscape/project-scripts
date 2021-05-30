ba70cec08976aec5b25a08dcd88c70f6961ad668
Sayanee <sayanee@gmail.com>
2021-05-30 15:19:47 +0800

    feat: add stats for pending items to buy in BOM

--- bom_stats.py
+++ bom_stats.py
@@ -6,6 +6,8 @@ filename = sys.argv[1]
 uniq_items = 0
 total_items = 0
 total_items_dnp = 0
+total_items_buy = 0
+total_items_buy_names = []
 grand_total_cost = 0.0
 vendor_list = []
 uniq_vendor_list = []
@@ -44,26 +46,38 @@ with open(filename, 'r') as csv_file:
 
     grand_total_cost += float(row[12])
 
-    if row[9] != "":
+    if row[9] != "":  # if vendor name exists
       vendor_list.append(str(row[9]))
+
+    if row[15] == "" and row[5] == "":
+      # if DNP is empty and internal stock is empty
+      # then the item is pending delivery, needs to be bought
+      total_items_buy += 1
+      total_items_buy_names.append(row[1])
 
     uniq_vendor_list = get_uniq_vendor_names(vendor_list)
     total_vendors = len(uniq_vendor_list)
 
-print("\nUniq Items:")
+print("\nUniq Items: ", end='')
 print(uniq_items)
-print("Total Items:")
+
+print("Total Items: ", end='')
 print(total_items)
-print("Total Items to populate:")
+print("Total Items to populate: ", end='')
 print(total_items - total_items_dnp)
-print("Total cost: $")
+
+print("Total cost: $", end='')
 print(round(grand_total_cost, 2))
-print("Vendor list:")
-print(vendor_list)
-print("Uniq vendor list:")
+
+print("Uniq vendor list: ", end='')
 print(uniq_vendor_list)
-print("Unique vendors:")
+
+print("Unique vendors: ", end='')
 print(total_vendors)
+
+print("Total items to buy: ", end='')
+print(total_items_buy)
+print(total_items_buy_names)
 
 # Calculate BOM analytics
 data = {}
@@ -73,6 +87,8 @@ data['bom-total-populate'] = total_items - total_items_dnp
 data['bom-cost'] = round(grand_total_cost, 2)
 data['vendor-names'] = uniq_vendor_list
 data['vendors'] = total_vendors
+data['bom-pending'] = total_items_buy
+data['bom-pending-names'] = total_items_buy_names
 
 with open('_data/bom.json', 'w') as outfile:
     json.dump(data, outfile, indent=2)
